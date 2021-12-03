a85393ff2945b8a5a57738048eb132eae05e9d83
Nick Fanton <51746234+n-fanton@users.noreply.github.com>
2021-12-03 20:39:21 +0000

    add detail option to list_routes
    detail option in list_routes will be used to show route details in a future function

--- R/list_routes.R
+++ R/list_routes.R
@@ -4,16 +4,21 @@
 #' Lothian Country, East Coast Buses, Nightbus,
 #' Nighthawk, and Edinburgh Trams services
 #'
+#' @param  .details Show route details?
+#' These are nested as lists within the tibble.
+#'
 #' @return A tibble
 #' @export
 #'
 #' @examples list_routes()
-list_routes <- function(){
+list_routes <- function(.details = FALSE){
   httr::GET(url = "https://tfe-opendata.com/api/v1/services") %>%
     httr::content(as = "text", encoding = "UTF-8") %>%
     jsonlite::fromJSON(flatten = TRUE) %>%
     magrittr::extract2(2) %>%
     tibble::as_tibble() %>%
     janitor::clean_names() %>%
-    dplyr::select(name, description, service_type)
+    {if (!.details) dplyr::select(.data = ., name, description, service_type)
+      else . } %>% View()
+
 }
