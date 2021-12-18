509d9a0c6b24a121e488d870c2462b72a2c5715d
Nick Fanton <51746234+n-fanton@users.noreply.github.com>
2021-12-18 22:21:08 +0000

    finalise live_departures

--- R/live_departures.R
+++ R/live_departures.R
@@ -10,19 +10,27 @@
 #' @examples live_departures(c(36235979, 36253636))
 live_departures = function(stop_code,
                            api = "https://tfe-opendata.com/api/",
-                           version = 1) {
+                           version = 1,
+                           show_bus = FALSE) {
 
-  stop_code <- as.character(stop_code)
+  if (show_bus) message("🚌")
+
+  stop_code <- stop_code %>%
+    as.character() %>%
+    stringr::str_replace_all("[:alpha:]", "")
+
   version   <- as.character(version)
 
-  stop_url <- paste0(api, "v", version, "/live_bus_times/")
+  stop_url  <- paste0(api, "v", version, "/live_bus_times/")
 
   all_stops_departures <- tibble::tibble()
 
   for (i in 1:length(stop_code)) {
 
+    url_for_this_stop <- paste0(stop_url, stop_code[i])
+
     this_stop_departures <-
-      httr::GET(url = paste0(stop_url, stop_code[i])) %>%
+      httr::GET(url = url_for_this_stop) %>%
       httr::content(as = "text", encoding = "UTF-8") %>%
       jsonlite::fromJSON(flatten = TRUE) %>%
       magrittr::extract2(2) %>%
